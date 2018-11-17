ac7204ccb12c64c32af341bc57bc0a36bcdf0522
Hampus Rude <hrude@kth.se>
2018-11-17 12:47:15 +0100

    Ändrat från svenska till engelska

--- app/forms.py
+++ app/forms.py
@@ -14,36 +14,36 @@ class RegistrationForm(FlaskForm):
 	def validate_email(self, email):
 		email = User.query.filter_by(email=email.data).first()
 		if email:
-			raise ValidationError('Mailadressen finns redan. Glömt Lösenordet? Annars välj en annan.')
+			raise ValidationError('The email already exists. Choose another one.')
 
 # Form som används för att ta emot input när en användare loggar in
 class LoginForm(FlaskForm):
 	email = StringField('Email', validators=[DataRequired(), Email()])
-	password = PasswordField('Lösenord', validators=[DataRequired()])
-	remember = BooleanField('Kom igåg mig')
-	submit = SubmitField('Logga in')
+	password = PasswordField('Password', validators=[DataRequired()])
+	remember = BooleanField('Remember me')
+	submit = SubmitField('Login')
 
 # Form för att uppdatera kontoinformation
 class UpdateAccountForm(FlaskForm):
-	old_password = PasswordField('Nuvarande lösenord', validators=[DataRequired()])
-	new_password = PasswordField('Nytt lösenord', validators=[DataRequired()])
-	confirm_password = PasswordField('Konfirmera nytt lösenord', validators=[DataRequired(), EqualTo('new_password')])
-	submit = SubmitField('Uppdatera lösenord')
+	old_password = PasswordField('Current password', validators=[DataRequired()])
+	new_password = PasswordField('New password', validators=[DataRequired()])
+	confirm_password = PasswordField('Confirm new password', validators=[DataRequired(), EqualTo('new_password')])
+	submit = SubmitField('Update password')
 
 # Form som används i reset password request
 class RequestResetForm(FlaskForm):
 	email = StringField('Email', validators=[DataRequired(), Email()])
-	submit = SubmitField('Skicka lösonordsåterställning')
+	submit = SubmitField('Send password reset')
 
 	# Kontrollera om email-adressen redan finns i databasen
 	def validate_email(self, email):
 		email = User.query.filter_by(email=email.data).first()
 		if email is None:
-			raise ValidationError('Det finns inget konto med den emailen. Du måste registrera dig först')
+			raise ValidationError('There is no account with that email, please register first.')
 
 # Input-form och knapp som finns när man återställer lösenord
 class ResetPasswordForm(FlaskForm):
-	password = PasswordField('lösenord', validators=[DataRequired()])
-	confirm_password = PasswordField('Bekräfta lösenord', 
+	password = PasswordField('Password', validators=[DataRequired()])
+	confirm_password = PasswordField('Confirm password',
 		validators=[DataRequired(), EqualTo('password')])
-	submit = SubmitField('Återställ lösenord')
+	submit = SubmitField('Reset password')
